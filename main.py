67250e3815293d466f22f6b13334d6d5de87bb04
Tasnim2403 <2294sa4728rumc@gmail.com>
2024-11-09 20:09:25 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+a = 5
+print(type(a))
+
+b = "5"
+print(type(b))
+
+c = 5.5
+print(type(c))
+
+d = int(input("Enter the value of d = "))
+e = int(input("Enter the value of e = "))
+
+temp = d
+d = e
+e = temp 
+
+print("D =",d)
+print("E =",e)
+
+f = 5
+g = 7
+print(f+g)
+
